af3fceceec09ee54ebd880a41e07ee4e8b9b9a2d
Nawab Khairuzzaman Mohammad Kibria <44573643+nawab69@users.noreply.github.com>
2020-07-23 16:41:52 +0600

    Header Component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import { Header } from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      
+    <Header />
+      <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
