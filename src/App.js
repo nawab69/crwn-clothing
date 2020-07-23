4fcfbf8268466d8af8d9637fc4588064609cc3df
Nawab Khairuzzaman Mohammad Kibria <44573643+nawab69@users.noreply.github.com>
2020-07-23 11:04:49 +0600

    React Router Added to Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
